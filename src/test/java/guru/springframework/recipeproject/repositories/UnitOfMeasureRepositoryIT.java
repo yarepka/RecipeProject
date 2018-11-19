982fc84edc2139980680bcfba3766f50dae4f03c
yarepka <yarepka01@gmail.com>
2018-11-19 17:46:50 +0300

    added Integration test for UnitOfMeasureRepository

---
+++ src/test/java/guru/springframework/recipeproject/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,34 @@
+package guru.springframework.recipeproject.repositories;
+
+import guru.springframework.recipeproject.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class) // running spring with Spring Context
+@DataJpaTest // used to test data layer with embedded database
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+
+    }
+}

6d96af61977f3015f20703b57621d7d7c58c5ba4
yarepka <yarepka01@gmail.com>
2018-12-13 16:22:22 +0300

    added recipeform and controller mapping for creating Recipe

--- src/main/java/guru/springframework/recipeproject/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeproject/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package guru.springframework.recipeproject.controllers;
 
+import guru.springframework.recipeproject.commands.RecipeCommand;
 import guru.springframework.recipeproject.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,6 +27,21 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }

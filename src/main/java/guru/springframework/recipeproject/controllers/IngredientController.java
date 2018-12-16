bdfbec7c7fbc11c2e8b58b25de2369d91d1ef01c
yarepka <yarepka01@gmail.com>
2018-12-16 17:25:25 +0300

    View Ingredients added

---
+++ src/main/java/guru/springframework/recipeproject/controllers/IngredientController.java
@@ -0,0 +1,30 @@
+package guru.springframework.recipeproject.controllers;
+
+import guru.springframework.recipeproject.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String list(@PathVariable String recipeId, Model model) {
+
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+
+        // use command object to avoid lazy load errors in Thymeleaf.
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
+
+}
